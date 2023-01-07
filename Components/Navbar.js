eba0cfbab2f38b1d1b0e66a603a24d21e7c8c385
Adeoye John Oluwakayode <Johnfixit293@gmail.com>
2023-01-07 18:33:10 +0100

    worked on the nav bar

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,99 +1,26 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Stepper from "@material-ui/core/Stepper";
-import Step from "@material-ui/core/Step";
-import StepLabel from "@material-ui/core/StepLabel";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import Script from "next/script";
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    height: "10vh"
-  },
-  button: {
-    marginRight: theme.spacing(1),
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-}));
-
-function getSteps() {
-  return ["Personal Detail", "Education", "Profile", "Employment", "Skill", "Langugae"];
-}
-
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return "Select campaign settings...";
-    case 1:
-      return "What is an ad group anyways?";
-    case 2:
-      return "This is the bit I really care about!";
-    default:
-      return "Unknown step";
-  }
-}
-
+import { useRouter } from "next/router";
+import { useState } from "react";
+import styled from "styled-components";
 function Navbar() {
-  const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
-  const steps = getSteps();
+  const [selectedIndex, setselectedIndex] = useState(undefined)
+  const router = useRouter()
+  const labels = [
+    "Personal_info",
+    "Education",
+    "Profile",
+    "Employment",
+    "Skill",
+    "Language",
+  ];
 
-  const isStepOptional = (step) => {
-    return step === 1;
-  };
-
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
-
-  const handleNext = () => {
-    // let newSkipped = skipped;
-    // if (isStepSkipped(activeStep)) {
-    //   console.log(activeStep)
-    //   newSkipped = new Set(newSkipped.values());
-    //   newSkipped.delete(activeStep);
-    // }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    // setActiveStep(4);
-    console.log(activeStep)
-    // setSkipped(newSkipped);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  // const handleSkip = () => {
-  //   if (!isStepOptional(activeStep)) {
-  //     // You probably want to guard against something like this,
-  //     // it should never occur unless someone's actively trying to break something.
-  //     throw new Error("You can't skip a step that isn't optional.");
-  //   }
-
-  //   setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  //   setSkipped((prevSkipped) => {
-  //     const newSkipped = new Set(prevSkipped.values());
-  //     newSkipped.add(activeStep);
-  //     return newSkipped;
-  //   });
-  // };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
-  // const Stepp =
+const changeLabel=(label, index)=>{
+    setselectedIndex(index)
+    router.push(`/build-cv/section/${label.toLowerCase()}`)
+}
 
   return (
-    <>
-    
-      <nav className="navbar navbar-expand-lg navbar-light fixed-to bg-light">
+    <Component>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-light">
         <div className="container">
           <a
             className="navbar-brand fw-bold fs-4 text-decoration-none"
@@ -118,66 +45,40 @@ function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li>
-                <div className={classes.root}>
-                  <Stepper activeStep={activeStep}>
-                    {steps.map((label, index) => {
-                      const stepProps = {};
-                      const labelProps = {};
-                      if (isStepSkipped(index)) {
-                        stepProps.completed = false;
-                      }
-                      return (
-                        <Step key={label} {...stepProps}>
-                          <StepLabel {...labelProps}>{label}</StepLabel>
-                        </Step>
-                      );
-                    })}
-                  </Stepper>
-                </div>
-              </li>
+              {labels.map((label, index) => {
+                return <li className={`nav-item rounded-pill ${index == selectedIndex&&"selected px-2 bg-color"}`} key={label} onClick={()=>changeLabel(label, index)}>
+                  <a
+                    className={`nav-link ${index == selectedIndex&&"px-2 bg-color"}`}
+                    href="#"
+                    tabindex="-1"
+                  >
+                    {label}
+                  </a>
+                </li>;
+              })}
             </ul>
           </div>
         </div>
       </nav>
-      <div>
-        {activeStep === steps.length ? (
-          <div>
-            <Typography className={classes.instructions}>
-              All steps completed - you&apos;re finished
-            </Typography>
-            <Button onClick={handleReset} className={classes.button}>
-              Reset
-            </Button>
-          </div>
-        ) : (
-          <div>
-            <Typography className={classes.instructions}>
-              {getStepContent(activeStep)}
-            </Typography>
-            <div>
-              <Button
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                className={classes.button}
-              >
-                Back
-              </Button>
-
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleNext}
-                className={classes.button}
-              >
-                {activeStep === steps.length - 1 ? "Finish" : "Next"}
-              </Button>
-            </div>
-          </div>
-        )}
-      </div>
-    </>
+    </Component>
   );
 }
 
 export default Navbar;
+
+const Component = styled.div`
+  nav{
+    .container{
+      .navbar-collapse{
+        ul{
+          display: flex;
+          gap: 3rem;
+          .selected{
+            transition: all 0.5s ease;
+          }
+        }
+
+      }
+    }
+  }
+`
